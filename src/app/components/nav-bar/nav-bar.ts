c283711c6b130a91752e8f1bef8b4a1ab6d7aac7
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-21 11:08:54 -0300

    criação da navbar com routerlinks e estilização

---
+++ src/app/components/nav-bar/nav-bar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-nav-bar',
+  styleUrl: './nav-bar.css',
+  templateUrl: './nav-bar.html',
+})
+export class NavBar {
+  
+}
