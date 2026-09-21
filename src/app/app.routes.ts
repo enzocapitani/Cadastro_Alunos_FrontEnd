13031cd0bfc612f0665a68ed1424454bf6eea601
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-21 11:05:46 -0300

    feat: adição das rotas para ter diversas abas ao projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AbaCadastro } from './components/aba-cadastro/aba-cadastro';
+import { AbaAlunos } from './components/aba-alunos/aba-alunos';
+import { AbaCursos } from './components/aba-cursos/aba-cursos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"cadastro",
+        component:AbaCadastro
+    },
+    {
+        path:"alunos",
+        component:AbaAlunos
+    },
+    {
+        path:"cursos",
+        component:AbaCursos
+    }
+];
