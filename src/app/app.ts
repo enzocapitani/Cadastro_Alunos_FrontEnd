fb1640aebddc17b4daf5be9bac0e53898f8a0ca3
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-21 11:06:09 -0300

    feat: adição do navbar e do router-outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AbaCadastro } from './components/aba-cadastro/aba-cadastro';
+import { NavBar } from './components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AbaCadastro],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
