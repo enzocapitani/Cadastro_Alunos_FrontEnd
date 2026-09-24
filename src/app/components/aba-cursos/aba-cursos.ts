24f6b4a78880f69f3d90f4377caaabbe25f91aac
Enzo Capitani <enzocapitanivieira@gmail.com>
2026-09-24 10:06:40 -0300

    feat: aba-cursos, adição somente de parte para visualizar os cursos

--- src/app/components/aba-cursos/aba-cursos.ts
+++ src/app/components/aba-cursos/aba-cursos.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Curso } from '../../types/Curso';
+import { CursoService } from '../../service/curso-service/curso-service';
 
 @Component({
   imports: [],
@@ -6,4 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './aba-cursos.css',
   templateUrl: './aba-cursos.html',
 })
-export class AbaCursos {}
+export class AbaCursos implements OnInit{
+
+  cursos: Curso[] = [];
+
+  constructor(private cursoService: CursoService, private cdr: ChangeDetectorRef){};
+  
+  async ngOnInit(){
+    const response = await this.cursoService.receberCursos();
+
+    this.cursos = response;
+    this.cdr.detectChanges();
+  }
+
+}
